ff40ed492880b10052b12402599bd715d70de7c5
Nikita-B05 <108193728+Nikita-B05@users.noreply.github.com>
2024-10-23 20:35:20 -0400

    fixed checkstyle

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,7 +27,15 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the currently logged-in user by their username.
+     * @param name the username of the user to set as the current user
+     */
     void setCurrentUser(String name);
 
+    /**
+     * Retrieves the username of the currently logged-in user.
+     * @return the username of the current user; null if no user is logged in
+     */
     String getCurrentUser();
 }
